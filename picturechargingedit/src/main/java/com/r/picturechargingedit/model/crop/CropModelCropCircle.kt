3072d109cf17bca7c67638d02b444f31924ab9bc
Roman Vysotsky <roman.vysotsky@zelfi.com>
2021-03-19 10:34:19 +0100

    [thumbnail-circle] only apply center logic for crop circle

--- picturechargingedit/src/main/java/com/r/picturechargingedit/model/crop/CropModelCropCircle.kt
+++ picturechargingedit/src/main/java/com/r/picturechargingedit/model/crop/CropModelCropCircle.kt
@@ -1,7 +1,10 @@
 package com.r.picturechargingedit.model.crop
 
+import android.graphics.RectF
 import com.r.picturechargingedit.EditPictureMode
+import com.r.picturechargingedit.arch.copyInto
 import com.r.picturechargingedit.model.picture.Picture
+import kotlin.math.abs
 
 /**
  *
@@ -24,6 +27,25 @@ class CropModelCropCircle(pictureModel: Picture) : CropModelThumb(pictureModel) 
 
     override fun getAspectRatio(): Float {
         return 1f
+    }
+
+    /**
+     * updates [bufferRect] in such a way, that its a square and in the center of [originalBoundsRect].
+     * Makes sure that the crop circle is centered initially
+     */
+    override fun updateInitialBoundsToBufferRect(bufferRect: RectF) {
+        val maxWidth = originalBoundsRect.width()
+        val maxHeight = originalBoundsRect.height()
+        val diff = abs(maxWidth - maxHeight) /2
+        originalBoundsRect.copyInto(bufferRect)
+
+        if(maxWidth < maxHeight) {
+            bufferRect.bottom = (bufferRect.bottom - diff).coerceAtLeast(0f)
+            bufferRect.top = (bufferRect.top + diff).coerceAtMost(bufferRect.bottom)
+        } else {
+            bufferRect.right = (bufferRect.right - diff).coerceAtLeast(0f)
+            bufferRect.left = (bufferRect.left + diff).coerceAtMost(bufferRect.right)
+        }
     }
 
 }
